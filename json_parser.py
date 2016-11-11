3c577bd0af282abca1b12e8185f6bcab110dc6b5
Sahil Jain <sahil@sahiljain.ca>
2016-11-10 20:34:25 -0500

    convert json files to csv

--- json_parser.py
+++ json_parser.py
@@ -1,50 +1,69 @@
 import json
 from pprint import pprint
 
-with open('10-general.json') as data_file:
-  data = json.load(data_file)
-  data = data["General"]
-  f = open('10-general.csv','w')
-  for i in range(len(data)):
-    line = ""
-    curTool = data[i]
+def convertToCsv(fileprefix):
+  with open(fileprefix + '.json') as data_file:
+    data = json.load(data_file)
+    category = data.keys()[0]
+    data = data[category]
+    f = open(fileprefix + '.csv','w')
+    for i in range(len(data)):
+      line = ""
+      curTool = data[i]
 
-    title = curTool["Name"]["text"]
-    line += title
-    line += "\t"
+      title = curTool["Name"]["text"]
+      line += title
+      line += "\t"
 
-    description = curTool["Description"]["text"]
-    line += description
-    line += "\t"
+      line += category
+      line += "\t"
 
-    link = curTool["Name"]["url"]
-    line += link
-    line += "\t"
+      if "Description" in curTool.keys():
+        description = curTool["Description"]["text"]
+      else:
+        description = title
+      line += description
+      line += "\t"
 
-    if "Author" in curTool.keys():
-      author = curTool["Author"]["text"]
-      line += author
+      link = curTool["Name"]["url"]
+      line += link
       line += "\t"
-      if "url" in curTool["Author"].keys():
-        authorlink = curTool["Author"]["url"]
-        line += authorlink
+
+      if "Author" in curTool.keys():
+        author = curTool["Author"]["text"]
+        line += author
+        line += "\t"
+        if "url" in curTool["Author"].keys():
+          authorlink = curTool["Author"]["url"]
+          line += authorlink
+        line += "\t"
+      elif "Authors" in curTool.keys():
+        author = curTool["Authors"]["text"]
+        line += author
+        line += "\t"
+        if "url" in curTool["Authors"].keys():
+          authorlink = curTool["Authors"]["url"]
+          line += authorlink
+        line += "\t"
+      else:
+        line += "\t"
+        line += "\t"
+      if "Source Code" in curTool.keys():
+        sourcelink = curTool["Source Code"]["url"]
+        line += sourcelink
       line += "\t"
-    elif "Authors" in curTool.keys():
-      author = curTool["Authors"]["text"]
-      line += author
-      line += "\t"
-      if "url" in curTool["Authors"].keys():
-        authorlink = curTool["Authors"]["url"]
-        line += authorlink
-      line += "\t"
-    else:
-      line += "\t"
-      line += "\t"
-    if "Source Code" in curTool.keys():
-      sourcelink = curTool["Source Code"]["url"]
-      line += sourcelink
-    line += "\t"
-    line += "approved"
-    print line
-    f.write(line + '\n') # python will convert \n to os.linesep
-  f.close() # you can omit in most cases as the destructor will call it
+      line += "approved"
+      print line
+      f.write(line.encode('utf8') + '\n') # python will convert \n to os.linesep
+    f.close() # you can omit in most cases as the destructor will call it
+
+convertToCsv("data/10-general")
+convertToCsv("data/20-course-selection")
+convertToCsv("data/30-jobmine")
+convertToCsv("data/40-learn")
+convertToCsv("data/40-learn")
+convertToCsv("data/45-grt")
+convertToCsv("data/50-class-helpers")
+convertToCsv("data/60-guides")
+convertToCsv("data/98-developers")
+convertToCsv("data/99-dead")
