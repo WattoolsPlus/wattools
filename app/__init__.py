71d453b1cb9ab33a262077294cd43026cb961597
jerryzxliu <jerryzxliu@users.noreply.github.com>
2016-11-10 21:32:10 -0500

    add custom encoder for enum

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,28 @@
 from flask import Flask
+from flask.json import JSONEncoder
 from app.routes import main
 from app.routes import api
 from app.database import db
+from app.tools.models import States
+
+
+# http://flask.pocoo.org/snippets/119/
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, States):
+                return str(obj.value)
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
 
 
 def create_app(object_name):
     app = Flask(__name__)
+    app.json_encoder = CustomJSONEncoder
 
     app.config.from_object(object_name)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
