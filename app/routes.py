b5c717e8111356adf56b25c0b0cc043224c11c6a
jerryzxliu <jerryzxliu@users.noreply.github.com>
2016-11-10 20:12:26 -0500

    setup tools model

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,44 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from database import db
+from tools.models import Tool
+from tools.models import States
 
 main = Blueprint('main', __name__)
+api = Blueprint('api', __name__)
 
 
+################################
+#               MAIN
+# ##############################
 @main.route('/')
 def home():
     return "Hello Wattools!"
+
+
+################################
+#               API
+# ##############################
+@api.route('/tools/')
+def get_tools():
+    return jsonify(
+        data=[tool.serialize() for tool in Tool.query.all()]
+    )
+
+
+@api.route('/tool/', methods=['POST'])
+def create_tool():
+    data = request.get_json()
+
+    # HACK so far we have no validation, need to do that later
+    tool = Tool(
+        title=data.get('title', ''),
+        description=data.get('description', ''),
+        author=data.get('author', None),
+        author_link=data.get('author_link', None),
+        link=data.get('link', ''),
+        source_link=data.get('source_link', None),
+        state=States.PENDING
+    )
+
+    db.session.add(tool)
+    db.session.commit()
