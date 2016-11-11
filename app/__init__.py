af5dd75e2a938d5b3394fc7264357527788823d6
jerryzxliu <jerryzxliu@users.noreply.github.com>
2016-11-10 21:11:05 -0500

    setup db

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from app.routes import main
 from app.routes import api
+from app.database import db
 
 
-def create_app():
+def create_app(object_name):
     app = Flask(__name__)
-    # app.config.from_object(object_name)
+
+    app.config.from_object(object_name)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
 
     app.register_blueprint(main)
     app.register_blueprint(api, url_prefix='/api')
