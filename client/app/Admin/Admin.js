797e57bd6aa4a446836303889bcf579940bb0b03
Parsa Alipour <parsaalipour@hotmail.com>
2016-11-10 22:48:09 -0500

    Admin panel properly displays response in tabular format

--- client/app/Admin/Admin.js
+++ client/app/Admin/Admin.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import { Table, Button } from 'react-bootstrap';
+import { Component } from 'react';
 
-export default React.createClass({
-  render: function(){
+export default class Admin extends React.Component {
+	constructor(props) {
+    super(props);
+
+    this.state = { tools: [] };
+    this.fetchTools();
+  }
+
+  fetchTools() {
+    fetch(`https://api.myjson.com/bins/23poo`).then(response => {
+      return response.json();
+    }).then(json => {
+      this.setState({ tools: json });
+    });
+  }
+
+  render(){
+
     return (
-      <div>
-        Hello World! Admin
-      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Description</th>
+            <th>Link</th>
+            <th>Author</th>
+            <th>Author Link</th>
+            <th>Source Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.state.tools.map(function(tool, i) {
+              return <tr key={tool.id}>
+                <td>{tool.title}</td>
+                <td>{tool.category}</td>
+                <td>{tool.description}</td>
+                <td>{tool.link}</td>
+                <td>{tool.author}</td>
+                <td>{tool.author_link}</td>
+                <td>{tool.source_link}</td>
+                <td> <Button>Approve</Button></td>
+                <td> <Button>Reject</Button></td>
+              </tr>;
+            })
+          }
+        </tbody>
+      </Table>
     )
   }
-});
+};
