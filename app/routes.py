929d77b0b48fd850be11147af62b0c83e05a3148
VVCepheiA <oyevera@gmail.com>
2016-11-12 13:06:38 -0500

    add auth for admin

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,31 @@ from flask import Blueprint, jsonify, request
 from database import db
 from tools.models import Tool
 from tools.models import States
+from functools import wraps
+from flask import request, Response
+import os
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == os.environ['ADMIN_PW']
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 main = Blueprint('main', __name__, static_folder='../client/dist/')
 api = Blueprint('api', __name__)
@@ -10,9 +35,13 @@ api = Blueprint('api', __name__)
 #               MAIN
 # ##############################
 @main.route('/')
-@main.route('/admin')
 @main.route('/submit')
 def home():
+    return main.send_static_file('index.html');
+
+@main.route('/admin')
+@requires_auth
+def admin():
     return main.send_static_file('index.html');
 
 @main.route('/bundle.js')
