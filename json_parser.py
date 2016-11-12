ec40b23c8b01ae1cc2b465804d4e81f8d882d0ac
Sahil Jain <sahil@sahiljain.ca>
2016-11-11 22:30:03 -0500

    update json parser and add new csv

--- json_parser.py
+++ json_parser.py
@@ -11,48 +11,48 @@ def convertToCsv(fileprefix):
       line = ""
       curTool = data[i]
 
-      title = curTool["Name"]["text"]
+      title = curTool["Name"]["text"].replace('\n', ' ')
       line += title
       line += "\t"
 
-      line += category
+      line += category.replace('\n', ' ')
       line += "\t"
 
       if "Description" in curTool.keys():
-        description = curTool["Description"]["text"]
+        description = curTool["Description"]["text"].replace('\n', ' ')
       else:
         description = title
       line += description
       line += "\t"
 
-      link = curTool["Name"]["url"]
+      link = curTool["Name"]["url"].replace('\n', ' ')
       line += link
       line += "\t"
 
       if "Author" in curTool.keys():
-        author = curTool["Author"]["text"]
+        author = curTool["Author"]["text"].replace('\n', ' ')
         line += author
         line += "\t"
         if "url" in curTool["Author"].keys():
-          authorlink = curTool["Author"]["url"]
+          authorlink = curTool["Author"]["url"].replace('\n', ' ')
           line += authorlink
         line += "\t"
       elif "Authors" in curTool.keys():
-        author = curTool["Authors"]["text"]
+        author = curTool["Authors"]["text"].replace('\n', ' ')
         line += author
         line += "\t"
         if "url" in curTool["Authors"].keys():
-          authorlink = curTool["Authors"]["url"]
+          authorlink = curTool["Authors"]["url"].replace('\n', ' ')
           line += authorlink
         line += "\t"
       else:
         line += "\t"
         line += "\t"
       if "Source Code" in curTool.keys():
-        sourcelink = curTool["Source Code"]["url"]
+        sourcelink = curTool["Source Code"]["url"].replace('\n', ' ')
         line += sourcelink
       line += "\t"
-      line += "approved"
+      line += "APPROVED"
       print line
       f.write(line.encode('utf8') + '\n') # python will convert \n to os.linesep
     f.close() # you can omit in most cases as the destructor will call it
