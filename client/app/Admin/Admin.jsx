63d0a9872b6a03f07dd96fc72d75766c0ffb2a2c
Sahil Jain <sahil@sahiljain.ca>
2016-11-12 20:43:58 -0500

    make Admin look nicer and fix lint

--- client/app/Admin/Admin.jsx
+++ client/app/Admin/Admin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, Button } from 'react-bootstrap';
+import { Grid, Table, Button } from 'react-bootstrap';
 
 export default class Admin extends React.Component {
   constructor(props) {
@@ -33,46 +33,54 @@ export default class Admin extends React.Component {
 
   render() {
     return (
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Category</th>
-            <th>Description</th>
-            <th>Link</th>
-            <th>Author</th>
-            <th>Author Link</th>
-            <th>Source Link</th>
-            <th>Approve</th>
-            <th>Reject</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            this.state.tools.map((tool) => (
-              <tr key={tool.id}>
-                <td>{tool.title}</td>
-                <td>{tool.category}</td>
-                <td>{tool.description}</td>
-                <td>{tool.link}</td>
-                <td>{tool.author}</td>
-                <td>{tool.author_link}</td>
-                <td>{tool.source_link}</td>
-                <td>
-                  <Button bsStyle="success" onClick={() => this.handleApprove(tool.id, 'approved')}>
-                    Approve
+      <Grid style={{ marginTop: '50px' }}>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Category</th>
+              <th>Description</th>
+              <th>Link</th>
+              <th>Author</th>
+              <th>Author Link</th>
+              <th>Source Link</th>
+              <th>Approve</th>
+              <th>Reject</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.tools.map((tool) => (
+                <tr key={tool.id}>
+                  <td>{tool.title}</td>
+                  <td>{tool.category}</td>
+                  <td>{tool.description}</td>
+                  <td>{tool.link}</td>
+                  <td>{tool.author}</td>
+                  <td>{tool.author_link}</td>
+                  <td>{tool.source_link}</td>
+                  <td>
+                    <Button
+                      bsStyle="success"
+                      onClick={() => this.handleApprove(tool.id, 'approved')}
+                    >
+                      Approve
+                    </Button>
+                  </td>
+                  <td>
+                    <Button
+                      bsStyle="danger"
+                      onClick={() => this.handleApprove(tool.id, 'rejected')}
+                    >
+                      Reject
                   </Button>
-                </td>
-                <td>
-                  <Button bsStyle="danger" onClick={() => this.handleApprove(tool.id, 'rejected')}>
-                    Reject
-                </Button>
-                </td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </Table>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </Table>
+      </Grid>
     );
   }
 }
